9a8cf82846886fb87ea3dea3acba1efd189ff3d5
fmcfar200 <fmcfar200@caledonian.ac.uk>
2018-09-12 22:33:15 +0100

    nav drawer

--- app/src/main/java/com/example/fraser/fndb/HomeActivity.java
+++ app/src/main/java/com/example/fraser/fndb/HomeActivity.java
@@ -2,13 +2,21 @@ package com.example.fraser.fndb;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
+    Toolbar toolbar;
     Button skinBtn;
 
     @Override
@@ -16,9 +24,34 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        toolbar = findViewById(R.id.toolbarID);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.nav_open, R.string.nav_closed)
+        {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                drawerView.bringToFront();
+            }
+        };
+
+
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         skinBtn = findViewById(R.id.skinButton);
         skinBtn.setOnClickListener(this);
     }
+
 
     @Override
     public void onClick(View v)
@@ -33,5 +66,24 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     {
         Intent i = new Intent(getApplicationContext(), theclass);
         startActivity(i);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.drHome)
+        {
+
+        }
+        else if (id == R.id.drSkins)
+        {
+            StartActivity(SeasonSelectActivity.class);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
