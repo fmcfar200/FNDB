4b47dfbbae5fe20f680cffb35a26378b68d14ee6
fmcfar200 <fmcfar200@caledonian.ac.uk>
2018-09-28 04:07:31 +0100

    scale

--- app/src/main/java/com/example/fraser/fndb/ShopGridAdapter.java
+++ app/src/main/java/com/example/fraser/fndb/ShopGridAdapter.java
@@ -2,20 +2,14 @@ package com.example.fraser.fndb;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
-
-import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -57,36 +51,29 @@ public class ShopGridAdapter extends BaseAdapter
         ImageView imageView = view.findViewById(R.id.sItemImage);
         TextView nameText = view.findViewById(R.id.sItemName);
 
-        FrameLayout costLayout = view.findViewById(R.id.costLayout);
         TextView costText = view.findViewById(R.id.costText);
 
         String rarity = data.get(position).rarity;
         if (rarity == "Uncommon")
         {
             rootLayout.setBackgroundResource(R.color.uncommonColor);
-            //costLayout.setBackgroundResource(R.color.uncommonColor);
 
         }
         else if (rarity == "Rare")
         {
             rootLayout.setBackgroundResource(R.color.rareColor);
-            //costLayout.setBackgroundResource(R.color.rareColor);
 
         }
         else if (rarity == "Epic")
         {
             rootLayout.setBackgroundResource(R.color.epicColor);
-            //costLayout.setBackgroundResource(R.color.rareColor);
 
         }
         else if (rarity == "Legendary")
         {
             rootLayout.setBackgroundResource(R.color.legendaryColor);
-            //costLayout.setBackgroundResource(R.color.rareColor);
 
         }
-
-        nameText.setBackgroundColor(getColorWithAlpha(R.color.blackAlpha,0.7f));
 
         Picasso.with(mContext).load(data.get(position).imageURL).into(imageView);
         nameText.setText(data.get(position).name);
@@ -98,6 +85,7 @@ public class ShopGridAdapter extends BaseAdapter
         return view;
     }
 
+    /*
     public static int getColorWithAlpha(int color, float ratio) {
         int newColor = 0;
         int alpha = Math.round(Color.alpha(color) * ratio);
@@ -107,4 +95,5 @@ public class ShopGridAdapter extends BaseAdapter
         newColor = Color.argb(alpha, r, g, b);
         return newColor;
     }
+    */
 }
